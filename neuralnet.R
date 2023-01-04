ffcc7d5eb0d64c6a5ed0c040d56d4b24abc03557
hyeonjoonpark <pjjoon1379@gmail.com>
2023-01-04 14:16:29 +0900

    R Start

--- neuralnet.R
+++ neuralnet.R
@@ -1,1 +1,17 @@
-install.packages("neralnet")
+install.packages("neuralnet", dependencies = T)
+install.packages("keras", dependencies = T)
+library(keras)
+library(neuralnet)
+
+head(infert)
+
+df <- infert[,2:6]
+table(df$case)
+
+model <- neuralnet(case~., data=df, hidden = c(2,3))
+model
+plot(model)
+
+compute(model, df[1,])
+
+
